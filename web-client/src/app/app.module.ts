d3b764c1948853dfee35ac00e1c14fe75bca1a05
Hiroon Wijekoon <hiroonwijekoon@gmail.com>
2022-08-13 14:26:00 +0530

    Search-box : UI

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-rounting.module';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-rounting.module";
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavComponent } from './components/shared/nav/nav.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
-import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
-import { CartComponent } from './components/shopping-cart/cart/cart.component';
-import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
-import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
-import { LoginComponent } from './components/login/login.component';
-import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
-import { RegisterComponent } from './components/register/register.component';
-import { RegisterSuccessComponent } from './components/shared/register-success/register-success.component';
-import { AccountVerifyComponent } from './components/shared/account-verify/account-verify.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/shared/header/header.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
+import { NavComponent } from "./components/shared/nav/nav.component";
+import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
+import { FiltersComponent } from "./components/shopping-cart/filters/filters.component";
+import { ProductListComponent } from "./components/shopping-cart/product-list/product-list.component";
+import { CartComponent } from "./components/shopping-cart/cart/cart.component";
+import { CartItemComponent } from "./components/shopping-cart/cart/cart-item/cart-item.component";
+import { ProductItemComponent } from "./components/shopping-cart/product-list/product-item/product-item.component";
+import { LoginComponent } from "./components/login/login.component";
+import { PageNotFoundComponent } from "./components/shared/page-not-found/page-not-found.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { RegisterSuccessComponent } from "./components/shared/register-success/register-success.component";
+import { AccountVerifyComponent } from "./components/shared/account-verify/account-verify.component";
+import { SearchBarComponent } from "./components/shopping-cart/search-bar/search-bar.component";
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import { AccountVerifyComponent } from './components/shared/account-verify/accou
     LoginComponent,
     RegisterComponent,
     RegisterSuccessComponent,
-    AccountVerifyComponent
+    AccountVerifyComponent,
+    SearchBarComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -46,9 +48,9 @@ import { AccountVerifyComponent } from './components/shared/account-verify/accou
     HttpClientModule,
     RouterModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
